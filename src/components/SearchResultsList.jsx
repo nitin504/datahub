e1d96ddef2be7d086f514b72593cc2761a97723c
nitin504 <nitinkumarp504@gmail.com>
2024-08-02 19:22:26 +0530

    initial commit

--- src/components/SearchResultsList.jsx
+++ src/components/SearchResultsList.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./SearchResultsList.css";
+import { SearchResult } from './SearchResult';
 
 export const SearchResultsList = ({ results }) => {
   return (
     <div className='results-list'>
         {results.map((result, id) => {
-                return <div key={id}>{result.name}</div>;
+                return <SearchResult key={id} result={result} />  ;
             })}
     </div>       
   );
