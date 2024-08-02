395370827f6b4bf5aa757ded4b996e9c1f668440
nitin504 <nitinkumarp504@gmail.com>
2024-08-02 12:53:26 +0530

    first commit

---
+++ src/components/SearchResultsList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./SearchResultsList.css";
+
+export const SearchResultsList = ({ results }) => {
+  return (
+    <div className='results-list'>
+        {results.map((result, id) => {
+                return <div key={id}>{result.name}</div>;
+            })}
+    </div>       
+  );
+}; 
